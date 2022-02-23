47ea5a42074f0950419ced800adb9a1e7e446a02
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-02-23 15:49:50 +0100

    feat: update USDC address

--- src/utilities/getUnderlyingTokenName.ts
+++ src/utilities/getUnderlyingTokenName.ts
@@ -4,7 +4,7 @@ export enum UnderlyingToken {
 
 const getUnderlyingTokenName = (address: string): UnderlyingToken => {
   switch (address) {
-    case '':
+    case '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48':
       return UnderlyingToken.USDC;
 
     default:
