e216284678c56fd5b926d7dc55e1fc1360be9307
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-03-24 12:42:20 +0100

    fix: add VAMM.handleUpgraded handler

--- src/mappings/vamm/index.ts
+++ src/mappings/vamm/index.ts
@@ -8,3 +8,4 @@ export { default as handleOwnershipTransferred } from './handleOwnershipTransfer
 export { default as handlePaused } from './handlePaused';
 export { default as handleSwap } from './handleSwap';
 export { default as handleUnpaused } from './handleUnpaused';
+export { default as handleUpgraded } from './handleUpgraded';
