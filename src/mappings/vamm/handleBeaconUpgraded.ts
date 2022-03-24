58921be3c22a4b39632f5d65d152927b8b667039
Nicholas Piano <nicholas.d.piano@gmail.com>
2022-03-24 11:23:36 +0100

    feat: update VAMM events

---
+++ src/mappings/vamm/handleBeaconUpgraded.ts
@@ -0,0 +1,5 @@
+import { BeaconUpgraded } from '../../../generated/templates/VAMM/VAMM';
+
+function handleBeaconUpgraded(_event: BeaconUpgraded): void {}
+
+export default handleBeaconUpgraded;
--- src/mappings/vamm/index.ts
+++ src/mappings/vamm/index.ts
@@ -1,3 +1,5 @@
+export { default as handleAdminChanged } from './handleAdminChanged';
+export { default as handleBeaconUpgraded } from './handleBeaconUpgraded';
 export { default as handleBurn } from './handleBurn';
 export { default as handleFeeSet } from './handleFeeSet';
 export { default as handleInitializeVAMM } from './handleInitializeVAMM';
