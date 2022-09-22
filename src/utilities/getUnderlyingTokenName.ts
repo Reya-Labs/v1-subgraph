9162d9d1a25001619f5129a928ef9842396adf9d
Ioana Roceanu <ioanaroceanu@Ioanas-MacBook-Pro-2.local>
2022-09-22 15:40:37 +0200

    feat: new goerli address for DAI

--- src/utilities/getUnderlyingTokenName.ts
+++ src/utilities/getUnderlyingTokenName.ts
@@ -24,6 +24,11 @@ const getUnderlyingTokenName = (address: string): string => {
     return 'DAI';
   }
 
+  // goerli 2
+  if (address.toLowerCase().includes('0xdc31Ee1784292379Fbb2964b3B9C4124D8F89C60'.toLowerCase())) {
+    return 'DAI';
+  }
+
   // mainnet
   if (address.toLowerCase().includes('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'.toLowerCase())) {
     return 'ETH';
